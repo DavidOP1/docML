137ad540057e95f0f6e8ff9c18f692fbe6144a96
DavidOP1 <54214707+DavidOP1@users.noreply.github.com>
2023-02-25 19:55:33 +0200

    Add files via upload

--- loop2.py
+++ loop2.py
@@ -1,6 +1,6 @@
 import os
 # Specify the path of the folder to iterate over
-folder_path_benign = "./Malicious MS Office documents dataset/benign/benign2"
+folder_path_benign = "benign2"
 folder_path_malware = "./malicious3"
 
 import oletools.oleid
@@ -15,10 +15,11 @@ from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import confusion_matrix, classification_report
 import matplotlib.pyplot as plt
 from sklearn import metrics, __all__, datasets
+import olefile
+class check_malware():
 
-
-def check(name, j,feature):
-    file_path = folder_path_benign+'/'+name
+ def check(self,name, j,feature,folder_path):
+    file_path = folder_path +'/'+name
     oid = oletools.oleid.OleID(file_path)
     key_list = {"Run": False, "Call": False, "PowerShell": False, "Hex Strings": False, "Kill": False,"Base64":False}
     output_types = {'AutoExec': False,  'Hex obfuscated': False, 'Base64 obfuscated': False,
@@ -27,27 +28,21 @@ def check(name, j,feature):
         indicators = oid.check()
     except:
         return
-    if name[0]=='b':
+    if name[0]=='b' and j!="check":
         print("Hello")
         feature["label"].append(0)
-    elif name[0]=='m':
+    elif name[0]=='m' and j!="check":
         feature["label"].append(1)
+
     myfile = file_path
     filedata = open(myfile, 'rb').read()
     vbaparser = VBA_Parser(myfile, data=filedata)
-    # for i in indicators:
-    #     if i.id =="vba":
-    #         #print('Indicator id=%s name="%s" type=%s value=%s' % (i.id, i.name, i.type, repr(i.value)))
+    feature["file size"].append(os.path.getsize(file_path))
     if vbaparser.detect_vba_macros():
-        # print("entered vba" ,)
-        # print('VBA Macros found= ',j, " name= ",name)
         results = vbaparser.analyze_macros()
         for kw_type, keyword, description in results:
-            # print('type=%s - keyword=%s - description=%s' % (kw_type, keyword, description))
-            check_type(kw_type, output_types,feature)
-            check_keyword(keyword, key_list,feature)
-            type_set.add(kw_type)
-            keyword_set.add(keyword)
+            self.check_type(kw_type, output_types,feature)
+            self.check_keyword(keyword, key_list,feature)
         for key, value in key_list.items():
             if value == False:
                 feature[key].append(0)
@@ -58,29 +53,25 @@ def check(name, j,feature):
         print(feature)
     else:
         print(' NOT VBA Macros found', j)
-        append_zero(feature)
+        self.append_zero(feature)
 
-    #print("list = " , key_list , " index = ",i)
-        # print('description:', i.description)
-        # print('')
+    return feature
 
-def append_zero(feature):
+ def append_zero(self,feature):
     val = ['AutoExec', 'Hex obfuscated', 'Base64 obfuscated', 'Malware', 'VBA Stomping','Executable', 'OLE', 'IOC',"Run", "Call", "PowerShell", "Hex Strings", "Kill"]
     for i in val:
         feature[i].append(0)
 
 
 
-def check_type(type, output_types,feature):
-    # Malware_types = {'Dridex': False, 'Ursnif': False, 'Zloader': False, 'Maldoc': False}
+ def check_type(self,type, output_types,feature):
     for key, val in output_types.items():
         if (type == key and val != True ) or type == 'Dridex' or type == 'Ursnif' or type == 'Zloader' or type == 'Maldoc' :
             output_types[key] = True
             feature[key].append(1)
             return
 
-
-def check_keyword(keyword, key_list,feature):
+ def check_keyword(self,keyword, key_list,feature):
     if keyword == 'Run' or keyword == 'run' and key_list["Run"] != True :
         key_list["Run"] = True
         feature["Run"].append(1)
@@ -105,32 +96,27 @@ def check_keyword(keyword, key_list,feature):
         feature["Base64"].append(1)
         return
 
-
-feature = {}
-data = set()
-type_set = set()
-keyword_set = set()
-
-def create_ml_model(feature):
+ def create_ml_model(self,feature):
         del feature["filename"]
         df = pd.DataFrame(feature)
         feature_list = df.drop("label",axis=1)
         feature_df = feature_list.to_numpy()
         label = np.stack(df["label"])
 
-        X_train, X_test, y_train, y_test = train_test_split(feature_df, label, test_size=0.1765, random_state=50, stratify=label)
+        X_train, X_test, y_train, y_test = train_test_split(feature_df, label, test_size=0.1, random_state=50, stratify=label)
         clf = RandomForestClassifier(n_estimators=100)
         clf.fit(X_train, y_train)
         y_pred = clf.predict(X_test)
-
+        # print(X_test)
+        # print(y_pred)
         # using metrics module for accuracy calculation
         print("ACCURACY OF THE MODEL: ", metrics.accuracy_score(y_test, y_pred)) #Uncomment for model accuracy.
         return clf, X_test, y_test
 
-# feature["IOC"]=[]
-def convert(data):
+ # feature["IOC"]=[]
+ def convert(self,data):
     # Open a new file in write mode
-    with open('data2.csv', 'w', newline='') as file:
+    with open('../data2.csv', 'w', newline='') as file:
         writer = csv.writer(file)
 
         # Write the header row
@@ -140,7 +126,7 @@ def convert(data):
         for row in zip(*data.values()):
             writer.writerow(row)
 
-def print_result_report(clf, X_test, y_test):
+ def print_result_report(clf, X_test, y_test):
     # We print our results
     sns.set(rc={'figure.figsize': (15, 8)})
     predictions = clf.predict(X_test)
@@ -154,7 +140,8 @@ def print_result_report(clf, X_test, y_test):
     print(clf_report)
 
 
-def run():
+ def run(self):
+    feature={}
     feature["filename"] = []
     feature["Call"] = []
     feature["Run"] = []
@@ -170,6 +157,7 @@ def run():
     feature["Executable"] = []
     feature["OLE"] = []
     feature["IOC"] = []
+    feature["file size"]=[]
     feature["label"]=[]
     #rename_files()
     i=0
@@ -177,12 +165,14 @@ def run():
      if os.path.isfile(os.path.join(folder_path_benign, filename)):
         #if file == 'doc' or file == 'xls':
         feature["filename"].append(filename)
-        check(filename, i,feature)
+        self.check(filename, i,feature,folder_path_benign)
         i += 1
 
-    for key in feature:
-        print("key = ",key," "," len = ",len(feature[key]))
-    clf,x,y=create_ml_model(feature)
+    clf,x,y=self.create_ml_model(feature)
     #print_result_report(clf,x,y)
-    convert(feature)
-run()
+    #self.convert(feature)
+    return clf
+# if __name__ == '__main__':
+#     mal = check_malware()
+#     mal.run()
+#size()
